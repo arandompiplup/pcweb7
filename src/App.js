44e7ec920a39b88aae9df346bf9d6a4a182fab78
arandompiplup <arandompiplup@gmail.com>
2024-08-07 14:24:18 +0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Homepage from './views/Homepage';
+import CreatePost from './views/CreatePost';
+import EditPost from './views/EditPost';
+import LoginPage from './views/LoginPage';
+import ProfilePage from './views/ProfilePage';
+import SearchResults from './views/SearchResults'
 
 function App() {
+
+
+  const router = createBrowserRouter(
+    [
+      // {path:"/", element:<PostPageHome />},
+      // {path:"/login", element:<LoginPage />},
+      // {path:"/signup", element:<SignUpPage />},
+      // {path:"/add", element:<PostPageAdd />},
+      // {path:"/post/:id", element:<PostPageDetails/>},
+      // {path:"/update/:id", element:<PostPageUpdate/>}
+      {path:"/", element:<Homepage/>},
+      {path:"/login", element:<LoginPage/>},
+      {path:"/profile/:id", element:<ProfilePage/>},
+      {path:"/create", element:<CreatePost/>},
+      {path:"/edit/:id", element:<EditPost/>},
+      {path:"/search/:query", element:<SearchResults/>},
+    ]
+  )
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/views/SearchResults.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { Container, Image, Nav, Navbar, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+
+export default function PostPageHome() {
+
+  return (
+    <>
+      <Navbar variant="light" bg="light">
+        <Container>
+          <Navbar.Brand href="/">Tinkergram pc6</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/add">New Post</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row>
+          <p>search page</p>
+        </Row>
+      </Container>
+    </>
+  );
+}
